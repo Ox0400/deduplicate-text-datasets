8075d62e893b8a77b21ae35d558c2a1b492ef6c3
zhipeng <5310853+Ox0400@users.noreply.github.com>
2023-08-17 15:37:12 +0800

    optimize parallel use ray

--- scripts/make_suffix_array.py
+++ scripts/make_suffix_array.py
@@ -18,6 +18,10 @@ import sys
 import multiprocessing as mp
 import numpy as np
 
+import ray
+from ray import remote
+
+
 data_size = os.path.getsize(sys.argv[1])
 
 HACK = 100000
@@ -25,12 +29,15 @@ HACK = 100000
 
 started = []
 
-if data_size > 10e9:
+if data_size > 10e10:
+    total_jobs = 1000
+    jobs_at_once = 48
+elif data_size > 10e9:
     total_jobs = 100
-    jobs_at_once = 20
+    jobs_at_once = 48
 elif data_size > 1e9:
     total_jobs = 96
-    jobs_at_once = 96
+    jobs_at_once = 20
 elif data_size > 10e6:
     total_jobs = 4
     jobs_at_once = 4
@@ -38,23 +45,28 @@ else:
     total_jobs = 1
     jobs_at_once = 1
 
+
 S = data_size//total_jobs
 
+@remote
+def make_part(fname, s, e):
+    cmd = f"./target/debug/dedup_dataset make-part --data-file {fname} --start-byte {s} --end-byte {e}"
+    print(cmd)
+    os.popen(cmd).read()
+    return s, e
 
 for jobstart in range(0, total_jobs, jobs_at_once):
     wait = []
     for i in range(jobstart,jobstart+jobs_at_once):
         s, e = i*S, min((i+1)*S+HACK, data_size)
-        cmd = "./target/debug/dedup_dataset make-part --data-file %s --start-byte %d --end-byte %d"%(sys.argv[1], s, e)
-        started.append((s, e))
-        print(cmd)
-        wait.append(os.popen(cmd))
-        
+        #cmd = "./target/debug/dedup_dataset make-part --data-file %s --start-byte %d --end-byte %d"%(sys.argv[1], s, e)
+        futures.append(make_part.remote(fname, s, e))
         if e == data_size:
             break
 
-    print("Waiting for jobs to finish")
-    [x.read() for x in wait]
+print("Waiting for jobs to finish")
+for i in ray.get(futures):
+    started.append(i)
 
 print("Checking all wrote correctly")
 
@@ -75,7 +87,7 @@ while True:
     time.sleep(1)
     if len(wait) == 0:
         break
-        
+
 
 print("Merging suffix trees")
 
